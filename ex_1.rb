4f0ec9e5c6e35e7317b32b4743fdc1c0cfaab959
crhel <helen.razinova@icloud.com>
2024-01-09 18:14:32 +0400

    Оптимизация цикла

--- ex_1.rb
+++ ex_1.rb
@@ -4,19 +4,18 @@ try_count = 10
 puts "Добро пожаловать в игру 'Пососи ка', #{name.chomp}! Угадай число от 1 до 100. У тебя #{try_count} попыток."
 number = rand(1..100)
 while try_count>0
+  try_count -=1
   puts "Введи предполагаемое число:"
   guess_number = gets.to_i
   if guess_number > 0 && guess_number < 100
     if guess_number == number 
-        puts "Нихуя ты красавчик, ты угадал с #{11 - try_count} попыток!" 
+        puts "Нихуя ты красавчик, ты угадал с #{10 - try_count} попыток!" 
         try_count = -1
     end
     if guess_number > number
-          try_count -=1
           puts "Ой, чето ты лишканул. Загадываемое число меньше. У тебя осталось #{try_count} попыток."
     end
     if guess_number < number
-          try_count -=1
           puts "Ой, чето ты не добрал. Загадываемое число больше. У тебя осталось #{try_count} попыток."
     end
   else
