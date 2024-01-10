6a330efdb10a446ad8a4455e53df332875a267f9
crhel <helen.razinova@icloud.com>
2024-01-10 16:50:17 +0400

    Цензура

--- ex_1.rb
+++ ex_1.rb
@@ -1,7 +1,7 @@
 puts "Введите ваше имя:"
 name = gets
 try_count = 10
-puts "Добро пожаловать в игру 'Пососи ка', #{name.chomp}! Угадай число от 1 до 100. У тебя #{try_count} попыток."
+puts "Добро пожаловать в игру 'Угадай ка', #{name.chomp}! Угадай число от 1 до 100. У тебя #{try_count} попыток."
 number = rand(1..100)
 while try_count>0
   try_count -=1
@@ -9,7 +9,7 @@ while try_count>0
   guess_number = gets.to_i
   if guess_number > 0 && guess_number < 100
     if guess_number == number 
-        puts "Нихуя ты красавчик, ты угадал с #{10 - try_count} попыток!" 
+        puts "Ого ты красавчик, ты угадал с #{10 - try_count} попыток!" 
         try_count = -1
     end
     if guess_number > number
@@ -19,10 +19,10 @@ while try_count>0
           puts "Ой, чето ты не добрал. Загадываемое число больше. У тебя осталось #{try_count} попыток."
     end
   else
-    puts "Чето ты хуйню написал. Я просил число. Пиздуй нахуй отсюда"
+    puts "Неправильный ввод. Попробуй в следующий раз!"
     try_count = -1
   end 
 end
 if try_count == 0
-    puts "Не расстраивайся. В следующий пососешь"
+    puts "Не расстраивайся. Попробуй в следующий раз!"
 end
