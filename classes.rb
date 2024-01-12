0ca9d99692c796ee994df53a6890c1b9e0c41693
crhel <helen.razinova@icloud.com>
2024-01-12 23:44:39 +0400

    adeed new instance variables - age, and new methods - report_age

--- classes.rb
+++ classes.rb
@@ -5,6 +5,10 @@ class Dog
     @name = "Sandy"
   end
 
+  def make_up_age
+    @age = 5
+  end
+
   def talk
     puts "#{@name} says Bark"
   end
@@ -12,11 +16,19 @@ class Dog
   def move(destination)
     puts "#{@name} runs to the #{destination}"
   end
+
+  def report_age
+    puts "#{@name} is #{@age} years old"
+  end
 end
 
 class Cat
   def make_up_name
     @name = "Lucy"
+  end
+
+  def make_up_age
+    @age = 2
   end
 
   def talk
@@ -26,11 +38,19 @@ class Cat
   def move(destination)
     puts "#{@name} runs to the #{destination}"
   end
+
+  def report_age
+    puts "#{@name} is #{@age} years old"
+  end
 end
 
 class Bird
   def make_up_name
     @name = "Chiki"
+  end
+
+  def make_up_age
+    @age = 4
   end
 
   def talk
@@ -40,21 +60,31 @@ class Bird
   def move(destination)
     puts "#{@name} flies to the #{destination}"
   end
+
+  def report_age
+    puts "#{@name} is #{@age} years old"
+  end
 end
 
 dog = Dog.new
 dog.make_up_name
+dog.make_up_age
 dog.talk
+dog.report_age
 dog.move("fence")
 
 cat = Cat.new
 cat.make_up_name
+cat.make_up_age
 cat.talk
+cat.report_age
 cat.move("litter box")
 
 bird = Bird.new
 bird.make_up_name
+bird.make_up_age
 bird.talk
+bird.report_age
 bird.move("feeder")
 
 # class Blender
