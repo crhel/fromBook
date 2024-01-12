2637056730582c375b627257f2e0c77be6b7383d
crhel <helen.razinova@icloud.com>
2024-01-12 22:01:57 +0400

    Перестановка фрагментов так, чтобы получилась работоспособная программа

--- classes.rb
+++ classes.rb
@@ -13,3 +13,17 @@ rex = Dog.new
 rex.talk
 rex.move("bowl")
 puts "#{rex.class}"
+
+class Blender
+  def close_lid
+    puts "Sealed tight!"
+  end
+
+  def blend(speed)
+    puts "Spinning on #{speed} setting."
+  end
+end
+
+blender = Blender.new
+blender.blend("high")
+blender.close_lid
