80af6f197050cf8649f87fe1477706c39c8fdef3
crhel <helen.razinova@icloud.com>
2024-01-12 16:17:15 +0400

    Изучение методов

---
+++ vehicle_methods.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+def accelerate
+  puts "Stepping on the gas"
+  puts "Speeding up"
+end
+
+def accelera; end
+
+def sound_horn
+  puts "Pressing the horn button"
+  puts "Beep beep!"
+end
+
+def use_headlights(brightness = "low-beam")
+  puts "Turning on #{brightness} headlights"
+  puts "Watch out for deer!"
+end
+
+def area_return(length, width)
+  length * width
+end
+
+def area(length, width)
+  length * width
+end
