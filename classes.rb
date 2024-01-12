5239eb108506901598dddab31eb9fa9a9df7edc0
crhel <helen.razinova@icloud.com>
2024-01-12 22:11:09 +0400

    Создание еще двух классов - Cat, Bird

--- classes.rb
+++ classes.rb
@@ -1,12 +1,32 @@
 # frozen_string_literal: true
 
 class Dog
-  def talk
-    puts "Bark"
+  def talk(name)
+    puts "#{name} says Bark"
   end
 
-  def move(destination)
-    puts "Running to the #{destination}"
+  def move(name, destination)
+    puts "#{name} runs to the #{destination}"
+  end
+end
+
+class Cat
+  def talk(name)
+    puts "#{name} says Meow"
+  end
+
+  def move(name, destination)
+    puts "#{name} runs to the #{destination}"
+  end
+end
+
+class Bird
+  def talk(name)
+    puts "#{name} says Chirp! Chirp!"
+  end
+
+  def move(name, destination)
+    puts "#{name} flies to the #{destination}"
   end
 end
 rex = Dog.new
